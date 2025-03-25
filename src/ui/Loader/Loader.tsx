6c2647cede875ef9ba7792131898333dd93489cb
Илья Копендох <ilakopendoh@MacBook-Air-Ila.local>
2025-03-25 19:29:45 +0300

    loader realised

--- src/ui/Loader/Loader.tsx
+++ src/ui/Loader/Loader.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import classes from './Loader.module.scss';
+// import classes from './Loader.module.scss';
+
+// const Loader = () => {
+//   return (
+//     <div className={classes.wrap}>
+//       <div className={classes.loader}>
+//         <div className={classes.box1}></div>
+//         <div className={classes.box2}></div>
+//         <div className={classes.box3}></div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Loader;
+
+import classes from './Loader.module.scss'; // Импортируем файл с CSS Modules
 
 const Loader = () => {
   return (
-    <div className={classes.wrap}>
-      <div className={classes.loader}>
-        <div className={classes.box1}></div>
-        <div className={classes.box2}></div>
-        <div className={classes.box3}></div>
-      </div>
+    <div className={classes.loaderWrapper}>
+      <div className={classes.loader}></div>
     </div>
   );
 };
